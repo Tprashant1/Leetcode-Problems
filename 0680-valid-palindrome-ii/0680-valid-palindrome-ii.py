66a5084afe6b8a828f3cbd06f7fdb05b4a152071
Tprashant1 <112966067+Tprashant1@users.noreply.github.com>
2023-06-17 23:50:55 +0530

    Time: 35 ms (99.79%), Space: 14.4 MB (36.63%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if (s==s[::-1]):
+            return True
+        i=0
+        j=len(s)-1
+        while(i<=j):
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return s[i+1:j+1] == s[i+1:j+1][::-1] or s[i:j] == s[i:j][::-1] 
+        return False
+               
