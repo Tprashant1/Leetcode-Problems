24e34787265eae0b6b41475a4126e2ce5d5ec715
Tprashant1 <112966067+Tprashant1@users.noreply.github.com>
2023-06-20 22:33:52 +0530

    Time: 3 ms (99.95%), Space: 13.2 MB (97.15%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,22 +1,15 @@
 class Solution(object):
+    def Square(self, n):
+        s = 0
+        while n != 0:
+            d = n % 10
+            s = s + d*d
+            n = n / 10
+        return s
+
     def isHappy(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        l=[]
-        while(n>0):
-            s=0
-            while(n>0):
-                d=n%10
-                s=s+d*d
-                n=n/10
-            if s in l:
-                return False
-            else:
-                l.append(s)
-            if s==1:
-                return True
-            n=s
-        return False
-            
+        l = set()
+        while n != 1 and n not in l:
+            l.add(n)
+            n = self.Square(n)
+        return n == 1
